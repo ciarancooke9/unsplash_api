03ed5083d29075275e8706b26d04dfc5dc3f322b
cookec9 <ciaran.cooke9@mail.dcu.ie>
2022-02-03 10:09:13 +0000

    Api working with reqres.in

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<h1>hello</h1>
+<?php
+
+$ch = curl_init();
+
+$url = "https://reqres.in/api/users?page=2";
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+
+if ($err = curl_error($ch)) {
+    echo $err;
+}
+else {
+    $decoded = json_decode($response, true);
+    echo '<pre>'; print_r($decoded); echo '</pre>';
+}
+foreach($decoded['data'] as $result) {
+    echo($result["first_name"]);
+    $avatar = $result["avatar"];
+    echo "<img src='{$avatar}'> <br>";
+}
+curl_close($ch);
+
+?>
